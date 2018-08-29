45854c6ff031bfca3393a9bcfdcad0d95dce36b9
Shlok Kashyap <shlokkashyap38@gmail.com>
2018-08-30 03:47:47 +0530

    Add files via upload

---
+++ arraySum.py
@@ -0,0 +1,8 @@
+def arraySum(l):
+    a = 0
+    for i in range(len(l)):
+        a = a + l[i]
+        
+    return(a)
+    
+        
---
+++ descending.py
@@ -0,0 +1,7 @@
+def decending(l):
+    a=sorted(l, reverse = True)
+    
+    if a == l:
+        return(True)
+    else:
+        return(False)
